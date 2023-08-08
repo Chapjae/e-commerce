ad24abd46b5b6e56f1f920aa1d01103ae550ca17
Jay Liu <jayliu89@gmail.com>
2023-08-07 18:06:26 -0700

    delete route for category completed

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -64,8 +64,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+try{
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+    
+  if (!categoryData[0]) {
+    res.status(404).json({message: "No category found"})
+    return;
+    }
+    res.status(200).json(categoryData);
+  }  catch {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
