7dfd9f0f99dab7b79e81c73ea700b71a836247a7
Jay Liu <jayliu89@gmail.com>
2023-08-08 18:06:50 -0700

    removed default readme file contents and starting to overwrite with correct readme content

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,8 +49,9 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body, {
-      where: {
+    const tagData = await Tag.update( 
+      { tag_name: req.body.tag_name },
+      { where: {
         id: req.params.id
       },
     });
